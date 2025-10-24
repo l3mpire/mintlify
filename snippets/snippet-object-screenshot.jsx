06f9135fcbb7b9e28b22868d8e64ca069ce8afc1
bvelitchkine <bvelitchkine@gmail.com>
2025-10-24 22:43:45 +0900

    ✨ linked obj & defs to lemlist interface

---
+++ snippets/snippet-object-screenshot.jsx
@@ -0,0 +1,10 @@
+export const SnippetObjectScreenshot = ({ objectName, filePath = null, extension = 'png' }) => {
+  return (
+    <>
+      <Frame caption={`${objectName[0].toUpperCase() + objectName.slice(1).toLowerCase()} screenshot from the lemlist app interface`}>
+        <img src={`/images/objects-definitions/${filePath ?? objectName}.${extension}`} alt={`${objectName[0].toUpperCase() + objectName.slice(1).toLowerCase()} screenshot from the lemlist app interface`} />
+      </Frame>
+    </>
+  )
+}
+
