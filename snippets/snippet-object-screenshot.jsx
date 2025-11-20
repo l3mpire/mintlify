add2fabda0459b5f4e682443a99058946fff6ea5
bvelitchkine <bvelitchkine@gmail.com>
2025-11-20 14:47:58 +0900

    fix: casing broke img links

--- snippets/snippet-object-screenshot.jsx
+++ snippets/snippet-object-screenshot.jsx
@@ -2,7 +2,7 @@ export const SnippetObjectScreenshot = ({ objectName, filePath = null, extension
   return (
     <>
       <Frame caption={`${objectName[0].toUpperCase() + objectName.slice(1).toLowerCase()} screenshot from the lemlist app interface`}>
-        <img src={`/images/objects-definitions/${filePath ?? objectName}.${extension}`} alt={`${objectName[0].toUpperCase() + objectName.slice(1).toLowerCase()} screenshot from the lemlist app interface`} />
+        <img src={`/images/objects-definitions/${filePath ?? objectName.toLowerCase()}.${extension}`} alt={`${objectName[0].toUpperCase() + objectName.slice(1).toLowerCase()} screenshot from the lemlist app interface`} />
       </Frame>
     </>
   )
